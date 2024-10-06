82429c0a2f819ad492e190d6b5091b67d365fef7
Jesús Monja <jmonjaron@gmail.com>
2024-10-06 01:39:30 -0500

    Update Desing Login - Interactions

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -23,76 +23,83 @@ const Login: React.FC<{ onLogin: (username: string) => void }> = ({ onLogin }) =
   };
 
   return (
-    <div className="flex min-h-screen bg-gray-100">
+    <div className="flex flex-col lg:flex-row h-screen bg-gray-100">
       {/* Contenedor del formulario de login */}
-      <div className="flex flex-col justify-center items-center lg:w-1/3 p-5 bg-white rounded-lg shadow-md space-y-6 max-w-md mx-auto"> 
-        <div className="rounded-full w-36 h-36 shadow-lg mb-3 overflow-hidden">
-          <img src={logo} alt="Logo" className="object-cover" />
+      <div className="flex flex-col w-full lg:w-1/3 p-4 bg-white shadow-md min-h-screen">
+        <div className="flex flex-col items-center justify-center w-full p-2 h-full">
+          <div className="flex flex-col items-center w-full max-w-xs space-y-6"> 
+            <div className="w-40 h-40 overflow-hidden">
+              <img src={logo} alt="Logo"/>
+            </div>
+
+            <h2 className="text-2xl text-center text-gray-800">Iniciar Sesión</h2> 
+
+            {errorMessage && (
+              <div className="text-red-500 text-center">{errorMessage}</div> // Mostrar el mensaje de error
+            )}
+
+            <form onSubmit={handleSubmit} className="w-full space-y-6">
+              <div>
+                <label htmlFor="username" className="block text-sm font-medium text-gray-700">Usuario</label>
+                <input
+                  id="usuario"
+                  name="usuario"
+                  type="text"
+                  required
+                  placeholder="Usuario"
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="mt-2 w-full p-3 border border-gray-500 rounded-md shadow appearance-none text-gray-700 leading-tight
+                  focus:outline-none focus:ring-2 focus:ring-gray-500"
+                />
+              </div>
+
+              <div className="relative">
+                <label htmlFor="password" className="block text-sm font-medium text-gray-700">Contraseña</label>
+                <input
+                  id="password"
+                  name="password"
+                  type={showPassword ? 'text' : 'password'}
+                  required
+                  placeholder="********"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="mt-2 w-full p-3 border border-gray-500 rounded-md shadow appearance-none text-gray-700 leading-tight
+                  focus:outline-none focus:ring-2 focus:ring-gray-500 focus:shadow-outline pr-10"
+                />
+                <button
+                  type="button"
+                  onClick={() => setShowPassword(!showPassword)}
+                  className="mt-1 absolute inset-y-12 right-3 flex items-center"
+                >
+                  {showPassword ? <FiEyeOff className="text-xl text-gray-500" /> : <FiEye className="text-xl text-gray-500" />}
+                </button>
+              </div>
+
+              <div className="flex justify-between items-center">
+                <a href="#" className="text-blue-500 hover:text-orange-500 text-base 
+                  hover:font-bold">Olvidaste tu contraseña?</a>
+              </div>
+
+              <div>
+                <button
+                  type="submit"
+                  className="w-full py-2 text-white bg-orange-500 rounded-md hover:bg-blue-500 hover:font-bold"
+                >
+                  Ingresar
+                </button>
+              </div>
+
+              <div className="text-center">
+                <span className="text-sm">¿No tienes cuenta? </span>
+                <a href="#" className="text-blue-500 hover:text-orange-500 text-base
+                    hover:font-bold">Obten HabitHub Ahora</a>
+              </div>
+            </form>
+          </div>
         </div>
-
-        <h2 className="text-2xl text-center text-gray-800">Iniciar Sesión</h2> 
-
-        {errorMessage && (
-          <div className="text-red-500 text-center">{errorMessage}</div> // Mostrar el mensaje de error
-        )}
-
-        <form onSubmit={handleSubmit} className="w-full space-y-5 px-10">
-          <div>
-            <label htmlFor="username" className="block text-sm font-medium text-gray-700">Usuario</label>
-            <input
-              id="username"
-              name="username"
-              type="text"
-              required
-              placeholder="Usuario"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              className="mt-2 block w-full p-3 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500"
-            />
-          </div>
-
-          <div className="relative">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-700">Contraseña</label>
-            <input
-              id="password"
-              name="password"
-              type={showPassword ? 'text' : 'password'}
-              required
-              placeholder="Contraseña"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="mt-2 block w-full p-3 border border-gray-300 rounded-md focus:ring-blue-500 focus:border-blue-500 pr-10"
-            />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="mt-1 absolute inset-y-12 right-3 flex items-center"
-            >
-              {showPassword ? <FiEyeOff className="text-xl text-gray-500" /> : <FiEye className="text-xl text-gray-500" />}
-            </button>
-          </div>
-
-          <div className="flex justify-between items-center">
-            <a href="#" className="text-blue-600 hover:underline text-sm">Olvidaste tu contraseña?</a>
-          </div>
-
-          <div>
-            <button
-              type="submit"
-              className="w-full bg-orange-500 hover:bg-orange-600 text-white font-semibold py-2 rounded-md transition duration-200"
-            >
-              Ingresar
-            </button>
-          </div>
-
-          <div className="text-center">
-            <span className="text-sm">¿No tienes cuenta? </span>
-            <a href="#" className="text-blue-600 hover:underline text-sm">Obten HabitHub Ahora</a>
-          </div>
-        </form>
-
         <div className="mt-auto text-center text-gray-500 text-sm">
-          © 2024 Mr Soft. Todos los derechos reservados.
+              © 2024 Mr Soft. Todos los derechos reservados.
         </div>
       </div>
 
